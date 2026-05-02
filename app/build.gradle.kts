64a3cf3b55179ec3e3e8819a781f69e110a4374c
Manpreet113 <manpreet10542@gmail.com>
2026-05-02 14:58:46 +0530

    chore: version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 37
         versionCode = 1
-        versionName = "0.1.2"
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
